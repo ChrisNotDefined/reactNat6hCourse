2cbe2e31af7d84545f0e7e15bae802607f6bd74b
ChrAP <christopherapstarwars3@gmail.com>
2020-06-07 22:51:26 -0500

    First Lecture

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding: 40}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput placeholder="Your course goal" style={{width: '80%', padding: 10, margin: 5, borderColor: 'black', borderWidth: 1}}/>
+        <Button title="ADD"/>
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
